d38bb597f4df8692eaea61234bd31a0084a6e39a
Rinzan <nprinzan@gmail.com>
2025-07-22 12:46:14 +0530

    style error fixed

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+// import "./App.css"
 // Register service worker for PWA
 // if ('serviceWorker' in navigator) {
 //   window.addEventListener('load', () => {
